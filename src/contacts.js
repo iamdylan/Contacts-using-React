c6a1e12e5416fc3aa4fb8f53cc2e8509017fa2ab
iamdylan <43369528+iamdylan@users.noreply.github.com>
2018-11-16 22:15:54 +0530

    added card component

---
+++ src/contacts.js
@@ -0,0 +1,62 @@
+export const contacts = [
+  {
+    id: 1,
+    name: 'Barry Allen',
+    username: 'Flash',
+    email: 'speedster@justice.org'
+  },
+  {
+    id: 2,
+    name: 'Matt Murdoch',
+    username: 'daredevil',
+    email: 'blind.lawyer@defenders.net'
+  },
+  {
+    id: 3,
+    name: 'Barbara Gordon',
+    username: 'batgirl',
+    email: 'demons.head@justice.org'
+  },
+  {
+    id: 4,
+    name: 'Bruce Banner',
+    username: 'hulk',
+    email: 'Strongest.avenger@avengers.org'
+  },
+  {
+    id: 5,
+    name: 'Natasha Romanoff',
+    username: 'blackwidow',
+    email: 'hulk.pacifier@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Dr. Stephen Strange',
+    username: 'Doctorstrange',
+    email: 'Sorcerer.supreme@illuminati.info'
+  },
+  {
+    id: 7,
+    name: 'Adam Warlock',
+    username: 'warlock',
+    email: 'cosmic.level@infinitywatch.net'
+  },
+  {
+    id: 8,
+    name: 'Dinah Drake',
+    username: 'blackcanary',
+    email: 'vocal.artist@justice.org'
+  },
+  {
+    id: 9,
+    name: 'Norrin Radd',
+    username: 'Silversurfer',
+    email: 'shinyman@theorder.info'
+  },
+  {
+    id: 10,
+    name: 'Stan Lee',
+    username: 'Creator',
+    email: 'legend@marvel.com'
+  }
+];
