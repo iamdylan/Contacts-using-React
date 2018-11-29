7df7b2fbd5ca72e5a38b548cdd9b635ae65edc92
iamdylan <43369528+iamdylan@users.noreply.github.com>
2018-11-29 22:22:11 +0530

    Added Scroll Component to CardList

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {contacts} from './contacts';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component{
@@ -25,7 +26,9 @@ class App extends Component{
 			<div className='tc'>
 				<h1 className='f1'>Contacts</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList contacts={filteredContacts}/>
+				<Scroll>
+					<CardList contacts={filteredContacts}/>
+				</Scroll>
 			</div>
 		);
 	}
