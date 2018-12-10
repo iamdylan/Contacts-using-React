96a6e50db9d41011949d98a281949fc1e3a3b99e
iamdylan <43369528+iamdylan@users.noreply.github.com>
2018-12-10 19:57:44 +0530

    Updates

--- src/contacts.js
+++ src/contacts.js
@@ -2,61 +2,101 @@ export const contacts = [
   {
     id: 1,
     name: 'Norrin Radd',
-    username: 'Silversurfer',
     email: 'shiny.man@theorder.info'
   },
   {
     id: 2,
     name: 'Adam Warlock',
-    username: 'warlock',
     email: 'cosmic.level@infinitywatch.net'
   },
   {
     id: 3,
     name: 'Barbara Gordon',
-    username: 'batgirl',
     email: 'demons.head@justice.org'
   },
   {
     id: 4,
     name: 'Bruce Banner',
-    username: 'hulk',
     email: 'Strongest.avenger@avengers.org'
   },
   {
     id: 5,
     name: 'Dinah Drake',
-    username: 'blackcanary',
     email: 'vocalist@justice.org'
   },
   {
     id: 6,
     name: 'Dr. Stephen Strange',
-    username: 'Doctorstrange',
     email: 'Sorcerer.supreme@illuminati.net'
   },
   {
     id: 7,
     name: 'Matt Murdoch',
-    username: 'daredevil',
     email: 'blind.lawyer@defenders.net'
   },
   {
     id: 8,
     name: 'Natasha Romanoff',
-    username: 'blackwidow',
     email: 'hulk.pacifier@shield.com'
   },
   {
     id: 9,
     name: 'Barry Allen',
-    username: 'Flash',
     email: 'speedster@justice.org'
   },
   {
     id: 10,
     name: 'Stan Lee',
-    username: 'Creator',
     email: 'thelegend@marvel.com'
-  }
+  },
+  {
+    id: 11,
+    name: 'Oliver Queen',
+    email: 'green-arrow@justice.org'
+  },
+  {
+    id: 12,
+    name: 'Marc Spector',
+    email: 'moon-knight@defenders.net'
+  },
+  {
+    id: 13,
+    name: 'Eddie Brock',
+    email: 'wearevenom@dark.avengers.net'
+  },
+  {
+    id: 14,
+    name: 'Mari McCabe',
+    email: 'vixen@justice.org'
+  },
+  {
+    id: 15,
+    name: 'Jessica Drew',
+    email: 'spiderwoman@secretavengers.org'
+  },
+  {
+    id: 16,
+    name: 'Roy Harper',
+    email: 'arsenal@outsiders.com'
+  },
+  {
+    id: 17,
+    name: 'Galan',
+    email: 'galactus@Eternitywatch.info'
+  },
+  {
+    id: 18,
+    name: 'Jason Todd',
+    email: 'redhood@outlaws.info'
+  },
+  {
+    id: 19,
+    name: 'Carol Danvers',
+    email: 'captainmarvel@shield.com'
+  },
+  {
+    id: 20,
+    name: 'Arthur Curry',
+    email: 'aquaman@justice.org'
+  },
 ];
