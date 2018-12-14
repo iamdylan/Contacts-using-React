37219c1db1d9972ea9adc0cc87639854a6ccca3a
iamdylan <43369528+iamdylan@users.noreply.github.com>
2018-12-14 14:42:05 +0530

    Redux: Added Connect() on App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,32 +1,43 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import {contacts} from '../contacts';
 import Scroll from '../components/Scroll';
 import './App.css';
 
+import {setSearchField} from '../actions';
+
+const mapStateToProps = (state) => {
+	return {
+		searchField: state.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
+
 class App extends Component{
 	constructor() {
 		super()
 		this.state = {
-			contacts: contacts,
-			searchfield: ''
+			contacts: contacts
 		}
 	}
 
-	onSearchChange = (event) => {
-		this.setState({searchfield: event.target.value})
-		
-	}
 	render(){
-		const {contacts, searchfield} = this.state;
+		const {contacts} = this.state;
+		const {searchField, onSearchChange} = this.props;
 		const filteredContacts = contacts.filter(contact =>{
-			return contact.name.toLowerCase().includes(searchfield.toLowerCase());
+			return contact.name.toLowerCase().includes(searchField.toLowerCase());
 		})
 		return(
 			<div className='tc'>
 				<h1 className='f1'>Contacts</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
+				<SearchBox searchChange={onSearchChange}/>
 				<Scroll>
 					<CardList contacts={filteredContacts}/>
 				</Scroll>
@@ -36,4 +47,4 @@ class App extends Component{
 	
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
